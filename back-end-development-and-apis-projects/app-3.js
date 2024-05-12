28999fe17506b3c6424683697993bd74a4c4836a
KenzArz <kenzart05@gmail.com>
2024-05-12 14:56:50 +0700

    make short_url unique

--- back-end-development-and-apis-projects/app-3.js
+++ back-end-development-and-apis-projects/app-3.js
@@ -1,45 +1,49 @@
 import dns from "dns";
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 app.use("/public", express.static(`${process.cwd()}/public`));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 const db = [];
 
 app.get("/", (req, res) => {
-	res.sendFile(process.cwd() + "/views/index.html");
+	res.sendFile(process.cwd() + "/public/index-3.html");
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+	const shorted = db.find(m => m.short_url == req.params.id);
+	if (shorted) {
+		return res.redirect(302, shorted.original_url);
+	}
+	return res.json({ error: "No short URL found for the given input" });
 });
 
 app.post("/api/shorturl", (req, res) => {
 	const url = req.body.url.match(/https:\/\/(www.)?.*/g);
-	if (!url) return res.json({ error: "invalid url" });
+	if (!url) return res.json({ error: "invalid URL" });
 
 	const validUrl = new URL(url[0]);
-	let result = {
-		original_url: validUrl.href,
+	const result = {
+		original_url: url[0],
 	};
 
-	dns.lookup(validUrl.hostname, (error, address, a) => {
+	dns.lookup(validUrl.hostname, (error, address) => {
 		if (error) return res.json(error);
-		const shorted = db.find(m => validUrl.href === m.original_url);
-		result.shorturl =
-			shorted?.shorturl ||
-			address.split(".").reduce((prev, current) => prev + Number(current), 0);
+		const shorted = db.find(m => url[0] === m.original_url);
+		result.short_url =
+			shorted?.short_url ||
+			Math.floor(Math.random() * Number(address.replaceAll(".", "")));
 
 		res.json(result);
 		if (!shorted) {
 			db.push({ ...result, address });
 		}
 	});
-});
-
-app.get("/api/shorturl/:id", (req, res) => {
-	const shorted = db.find(m => m.shorturl == req.params.id);
-	if (!shorted)
-		return res.json({ error: "No short URL found for the given input" });
-	res.redirect(shorted.original_url);
 });
 
 app.listen(3000, () => {
